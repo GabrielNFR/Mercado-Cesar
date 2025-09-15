43a36d71718911c61451f30a960645d383558bd0
Gabriel Rocha <skicklel@gmail.com>
2025-09-15 17:47:08 -0300

    Criada classe Produto

--- mercadocesar/models.py
+++ mercadocesar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 
+class Produto(models.Model):
+	codigo = models.CharField(max_length=30, unique=True)
+	categoria = models.CharField(max_length=50)
+	preco_custo = models.DecimalField(max_digits=10, decimal_places=2)
+	preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
+	unidade_medida = models.CharField(max_length=20)
+
+	def __str__(self):
+		return f"{self.codigo} - {self.categoria}"
+
