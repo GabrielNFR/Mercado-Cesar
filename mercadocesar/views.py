3c05fadfbb2410ea9094af96a4fb9c6fee9316bd
Gabriel Rocha <skicklel@gmail.com>
2025-09-21 15:35:21 -0300

    Criada view para visualização do estoque baixo, e adicionada sua url, e uma template simples

--- mercadocesar/views.py
+++ mercadocesar/views.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
 from django.shortcuts import render, redirect
+from .models import Estoque
 
 @login_required
 def home(request):
@@ -15,3 +16,8 @@ def register(request):
     else:
         form = UserCreationForm()
     return render(request, 'registration/register.html', {'form': form})
+
+@user_passes_test(lambda u: u.is_staff)
+def estoque_baixo(request):
+    estoques = Estoque.abaixo_estoque_minimo()
+    return render(request, 'estoque_baixo.html', {'estoques': estoques})
