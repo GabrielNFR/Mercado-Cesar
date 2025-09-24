6e3f4cfa09852bdf8b5706e0f499f0d71a61346c
Gabriel Rocha <skicklel@gmail.com>
2025-09-24 18:42:31 -0300

    Adicionado atributo descrição a classe Produto nos modelos, e atualizadas suas referências

--- mercadocesar/models.py
+++ mercadocesar/models.py
@@ -3,13 +3,14 @@ from django.db import models
 
 class Produto(models.Model):
 	codigo = models.CharField(max_length=30, unique=True)
+	descricao = models.CharField(max_length=200, blank=True, default="")
 	categoria = models.CharField(max_length=50)
 	preco_custo = models.DecimalField(max_digits=10, decimal_places=2)
 	preco_venda = models.DecimalField(max_digits=10, decimal_places=2)
 	unidade_medida = models.CharField(max_length=20)
 
 	def __str__(self):
-		return f"{self.codigo} - {self.categoria}"
+		return f"{self.codigo} - {self.descricao} - {self.categoria}"
 
 
 class Armazem(models.Model):
