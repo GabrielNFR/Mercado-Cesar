78006f4e594c99e366c282b77c4ceb31d6751d47
Gabriel Rocha <skicklel@gmail.com>
2025-11-20 01:32:43 -0300

    Adicionados modelos a página admin para debug

--- mercadocesar/admin.py
+++ mercadocesar/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
-from .models import Produto, Estoque, Armazem, ItemCarrinho,Pedido
+from .models import Produto, Estoque, Armazem, ItemCarrinho, Pedido, ItemPedido
 
-# Comentado: usando views customizadas no lugar do Django Admin
-# admin.site.register(Produto)
-# admin.site.register(Estoque)
-# admin.site.register(Armazem)
+admin.site.register(Produto)
+admin.site.register(Estoque)
+admin.site.register(Armazem)
+
+class ItemPedidoInline(admin.TabularInline):
+    model = ItemPedido
+    extra = 0
+    readonly_fields = ('preco_unitario',)
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'tipo_entrega', 'data_criacao', 'calcular_total')
+    list_filter = ('tipo_entrega', 'data_criacao')
+    search_fields = ('usuario__username', 'id')
+    inlines = [ItemPedidoInline]
+    readonly_fields = ('data_criacao',)
+
+@admin.register(ItemPedido)
+class ItemPedidoAdmin(admin.ModelAdmin):
+    list_display = ('pedido', 'produto', 'quantidade', 'preco_unitario')
+
 
